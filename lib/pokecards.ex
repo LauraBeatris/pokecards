eeaec9121b0a40a86f6fdb33fc3bfbc457d2f44d
Laura <laurabeatriserafim@gmail.com>
2020-06-18 00:02:06 -0300

    feat: shuffle deck and verify if it constains a pokemon

--- lib/pokecards.ex
+++ lib/pokecards.ex
@@ -2,4 +2,12 @@ defmodule Pokecards do
   def create_deck do
     ["Pikachu", "Bulbasaur", "Piplup"]
   end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def constains?(deck, pokemon) do
+    Enum.member?(deck, pokemon)
+  end
 end
