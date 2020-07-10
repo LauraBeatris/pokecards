3129b08e879ef340138c78fafd112eaf4ec24d74
Laura <laurabeatriserafim@gmail.com>
2020-07-10 10:23:50 -0300

    tests: add unit tests for create_deck and shuffle

--- test/pokecards_test.exs
+++ test/pokecards_test.exs
@@ -2,7 +2,17 @@ defmodule PokecardsTest do
   use ExUnit.Case
   doctest Pokecards
 
-  test "greets the world" do
-    assert Pokecards.hello() == :world
+  describe "Deck Manipulation" do
+    @describetag :pokecard
+
+    test "should create 15 cards" do
+      deck_length = length(Pokecards.create_deck)
+      assert deck_length == 15
+    end
+
+    test "should shuffle a deck" do
+      deck = Pokecards.create_deck
+      refute deck == Pokecards.shuffle(deck)
+    end
   end
 end
