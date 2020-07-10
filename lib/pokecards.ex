6e32924b4c02b0b8b107e8ecbe8b0b240537a233
Laura <laurabeatriserafim@gmail.com>
2020-07-10 09:38:32 -0300

    docs: add documentation to pokecards module and pokemon struct

--- lib/pokecards.ex
+++ lib/pokecards.ex
@@ -1,6 +1,20 @@
 defmodule Pokecards do
+  @moduledoc """
+    Provides functions to create and handle a deck of pokemons
+  """
+  @moduledoc since: "0.1.0"
+
   require Pokemon
 
+  @doc """
+  Creates a deck of pokemons
+
+  ## Examples
+
+      iex> deck = Pokecards.create_deck
+      iex> deck
+  """
+  @doc since: "0.1.0"
   def create_deck do
     pokemons = [
       %Pokemon{name: "Pikachu", ability: "Static"},
@@ -14,19 +28,59 @@ defmodule Pokecards do
     end
   end
 
+  @doc """
+  Shuffles a deck of pokemons
+
+  ## Examples
+
+      iex> deck = Pokecards.create_deck
+      iex> deck = Pokecards.shuffle(deck)
+      iex> deck
+  """
+  @doc since: "0.1.0"
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+
+  @doc """
+  Verifies if a `deck` constains a `pokemon`
+
+  ## Examples
+
+      iex> deck = Pokecards.create_deck
+      iex> Pokecards.constains?(deck, "Name: Piplup, Ability: Torrent, Damage: 50")
+      true
+  """
   def constains?(deck, pokemon) do
     Enum.member?(deck, pokemon)
   end
 
+  @doc """
+  Deal a deck of pokemons by a given a `hand_size`
+
+  ## Examples
+
+      iex> deck = Pokecards.create_deck
+      iex> { hand, _rest_of_hand } = Pokecards.deal(deck, 2)
+      iex> length(hand) == 2
+      true
+  """
   def deal(deck, hand_size) do
-    { hand, _rest_of_deck } = Enum.split(deck, hand_size)
-    hand
+    Enum.split(deck, hand_size)
   end
 
+  @doc """
+  Saves a ``deck`` with a given ``deckname`` to the filesystem
+
+  Returns `:ok`
+
+  ## Examples
+
+      iex> deck = Pokecards.create_deck
+      iex> Pokecards.save("cardname", deck)
+      :ok
+  """
   def save(deckname, deck) do
     try do
       binary = :erlang.term_to_binary(deck)
@@ -36,6 +90,13 @@ defmodule Pokecards do
     end
   end
 
+  @doc """
+  Loads a deck with the given ``deckname`` from the filesystem
+
+  ## Examples
+      iex> deck = Pokecards.load("test")
+      iex> deck
+  """
   def load(deckname) do
     try do
       case File.read("decks/#{deckname}") do
@@ -47,13 +108,23 @@ defmodule Pokecards do
     end
   end
 
+  @doc """
+  Returns a hand of a pokemon deck based on a `hand_size`
+
+  ## Examples
+      iex> hand = Pokecards.create_hand(5)
+      iex> length(hand) == 5
+      true
+  """
   def create_hand(hand_size) do
     if (!hand_size) do
       raise "You must provide the size of the deck hand"
     end
 
-    Pokecards.create_deck
+    { hand, _rest_of_hand } = Pokecards.create_deck
     |> Pokecards.shuffle
     |> Pokecards.deal(hand_size)
+
+    hand
   end
 end
--- lib/pokemon.ex
+++ lib/pokemon.ex
@@ -1,3 +1,8 @@
 defmodule Pokemon do
+  @moduledoc """
+    Provides a model of a Pokemon
+  """
+  @moduledoc since: "0.1.0"
+
   defstruct name: "", ability: ""
 end
